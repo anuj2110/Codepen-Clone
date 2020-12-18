ac37c45e0a1dc2b78eb79d3acea71590757c67e3
anuj2110 <atrehan789@gmail.com>
2020-12-18 19:54:15 +0530

    Initial app

---
+++ src/components/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor';
+import useLocalStorage from '../hooks/useLocalStorage'
+function App() {
+  const [html, setHtml] = useLocalStorage('html','');
+  const [javascript, setJavascript] = useLocalStorage('css','');
+  const [css, setCss] = useLocalStorage('js','');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${javascript}</script>
+      </html>
+    `)
+    },250)
+    
+    return () =>clearTimeout(timeout)
+  }, [html, css, javascript])
+
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor language="javascript"
+          displayName="Javascript"
+          value={javascript}
+          onChange={setJavascript}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameborder="0"
+          width="100%"
+          height="100%" />
+      </div>
+    </>
+  );
+}
+
+export default App;
